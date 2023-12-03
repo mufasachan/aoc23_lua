9648c397dd160e688392718ae63e298a6b48dfec
mufasachan <mufasachan@hotmail.com>
2023-12-03 03:20:56 +0100

    more performance

--- 02/challenge1.lua
+++ 02/challenge1.lua
@@ -1,51 +1,62 @@
 local find_colour_number = require("common").find_colour_number
 
-local file = io.open("./input", "r")
 
-if not file then
-	os.exit(1)
-end
+local function run()
+	local file = io.open("./input", "r")
 
+	if not file then
+		os.exit(1)
+	end
 
-local colours_to_max = {
-	["red"] = 12,
-	["green"] = 13,
-	["blue"] = 14,
-}
+	local t1 = os.clock()
 
-local game_ids_sum = 0
-for line in file:lines() do
-	-- Find the id
-	local game_id = tonumber(line:match("%d+"))
+	local colours_to_max = {
+		["red"] = 12,
+		["green"] = 13,
+		["blue"] = 14,
+	}
 
-	-- Init 
-	local start_idx = line:find(":")
-	local end_idx = line:find(";")
+	local game_ids_sum = 0
+	for line in file:lines() do
+		-- Find the id and init indices
+		local _, start_idx, game_id = line:find("Game (%d+):")
+		local end_idx = line:find(";", start_idx)
 
-	local line_is_ok = true
-	while start_idx and line_is_ok do
-		for colour, max_value in pairs(colours_to_max) do
-			local colour_number = find_colour_number(line, colour, start_idx, end_idx)
+		while start_idx do
+			for colour, max_value in pairs(colours_to_max) do
+				local colour_number = find_colour_number(line, colour, start_idx, end_idx)
 
-			if colour_number then
-				if colour_number > max_value then
-					line_is_ok = false
+				if colour_number and colour_number > max_value then
+					goto next_line
 				end
 			end
-		end
 
-		start_idx = end_idx
-		if start_idx then
+			start_idx = end_idx
+
+			if not end_idx then
+				goto last_set
+			end
+
 			end_idx = line:find(";", start_idx + 1)
+
+			::last_set::
 		end
+
+		game_ids_sum = game_ids_sum + game_id
+
+		::next_line::
 	end
 
-	if line_is_ok then
-		game_ids_sum = game_ids_sum + game_id
-	end
+	local t2 = os.clock()
+	file:close()
+	return t2 - t1
+
 
 end
 
-print(game_ids_sum)
-
-file:close()
+local mean = 0
+local iter = 500
+for _ = 1, iter do
+	mean = mean + run()
+end
+print(mean / iter * 1000 .. " ms")
