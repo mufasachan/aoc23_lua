308e863c0465e95689bda7a49d8c3c0c79113cb0
mufasachan <mufasachan@hotmail.com>
2023-12-17 03:18:20 +0100

    custom copy and equal

--- lib.lua
+++ lib.lua
@@ -4,7 +4,6 @@ local lib = {}
 ---@alias list T[] a 1D continuous 1-indexed
 ---@alias matrix list[] a 2D continuous 1-indexed
 
-
 ---@param list list The list to be printed
 ---@param separator? string The seperator between elements
 function lib.lprint(list, separator)
@@ -29,39 +28,57 @@ function lib.mprint(matrix, separator, row_separator)
 	print(table.concat(rows, row_separator))
 end
 
+local function _copy(e)
+	return e
+end
+
 ---Copy every value from `list`.
+---@generic T_cpy # type to be copied
 ---@param list list 1D list, continuous 1-indexed
+---@param copy? fun(e: T_cpy):T_cpy Copy element function
 ---@return list 1D list with the same values as `list`
-function lib.lcopy(list)
-	local copy = {}
+function lib.lcopy(list, copy)
+	copy = copy or _copy
+
+	local _list = {}
 	for i = 1, #list do
-		copy[#copy + 1] = list[i]
+		_list[#_list + 1] = copy(list[i])
 	end
-	return copy
+	return _list
 end
 
 ---Copy every value from `matrix`.
+---@generic T_cpy # type to be copied
 ---@param matrix matrix 2D matrix, continuous 1-indexed
+---@param copy? fun(e: T_cpy):T_cpy Copy element function
 ---@return matrix 2D matrix with the same values as `matrix`
-function lib.mcopy(matrix)
-	local copy = {}
+function lib.mcopy(matrix, copy)
+	copy = copy or _copy
+
+	local _matrix = {}
 	for i = 1, #matrix do
-		copy[#copy + 1] = lib.lcopy(matrix[i])
+		_matrix[#_matrix + 1] = lib.lcopy(matrix[i], copy)
 	end
-	return copy
+	return _matrix
 end
 
+local function _equal(a, b)
+	return a == b
+end
 ---Tests equalities of two lists
 ---@param list1 list 1D table, continuous 1-D
 ---@param list2 list 1D table, continuous 1-D
+---@param equal? fun(e1, e2): boolean Function equality
 ---@return boolean #return `true` if `list1` == `list2` (values), else `false`
-function lib.lequal(list1, list2)
+function lib.lequal(list1, list2, equal)
+	equal = equal or _equal
+
 	if #list1 ~= #list2 then
 		error("Lists should have the same size to be compared")
 	end
 
 	for i = 1, #list1 do
-		if list1[i] ~= list2[i] then
+		if not equal(list1[i], list2[i]) then
 			return false
 		end
 	end
@@ -72,14 +89,17 @@ end
 ---Tests equalities of two matrix
 ---@param matrix1 matrix 2D table, continuous 2-D
 ---@param matrix2 matrix 2D table, continuous 2-D
+---@param equal? fun(e1, e2): boolean Function equality
 ---@return boolean #return `true` if `matrix1` == `matrix2` (values), else `false`
-function lib.mequal(matrix1, matrix2)
+function lib.mequal(matrix1, matrix2, equal)
+	equal = equal or _equal
+
 	if #matrix1 ~= #matrix2 then
 		error("Matrixes should have the same size to be compared")
 	end
 
 	for i = 1, #matrix1 do
-		local error_code, rows_is_equal = pcall(lib.lequal, matrix1[i], matrix2[i])
+		local error_code, rows_is_equal = pcall(lib.lequal, matrix1[i], matrix2[i], equal)
 		if not error_code then
 			error("Matrixes should have the same size to be compared")
 		end
